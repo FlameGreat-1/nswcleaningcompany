321f001d42d7249ba7ddc3628d538ee377dd5ec3
Iziogo Emmanuel Ugochukwu <eugochukwu77@gmail.com>
2025-08-26 19:16:51 +0100

    Update urls.py

--- cleaning_service/urls.py
+++ cleaning_service/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
@@ -54,6 +54,13 @@ api_urlpatterns = [
 
 urlpatterns = api_urlpatterns
 
+urlpatterns += [
+    re_path(
+        r"^api/api/(?P<path>.*)$",
+        RedirectView.as_view(url="/api/%(path)s", permanent=False),
+    ),
+]
+
 if settings.DEBUG:
     try:
         import debug_toolbar
@@ -65,7 +72,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# This catch-all route must be last
 urlpatterns.append(re_path(r".*", TemplateView.as_view(template_name="index.html")))
 
 handler404 = "django.views.defaults.page_not_found"
